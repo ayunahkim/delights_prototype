14b20f0e458420862b73a61fd989eb5a803e3188
ayunahkim <79731952+ayunahkim@users.noreply.github.com>
2026-03-26 18:47:37 +0900

    constellation example 1 added

--- src/sketch.js
+++ src/sketch.js
@@ -8,7 +8,9 @@ let textboxes = [];
 let star1,star2 = false;
 let textbox = 'none';
 let col1, col2;
+let const1 = false;
 
+let const1star1;
 
 function preload(){
   bg = loadImage('/assets/bg.png')
@@ -21,6 +23,8 @@ function preload(){
 
   glows[1] = loadImage('/assets/glow1.png')
   glows[2] = loadImage('/assets/glow2.png')
+
+  const1star1 = loadImage('/assets/pokopiacalculator.gif');
 }
 
 function setup() {
@@ -36,7 +40,10 @@ function setup() {
   col2.setAlpha(170);
 
   stars[1].resize(42,46)
+  stars[2].resize(66,71)
   constellations[1].resize(236,217)
+
+  const1star1.resize(300,300)
 
 }
 
@@ -89,10 +96,18 @@ function hoverGlow(){
 }
 
 function mouseClicked(){
-  if(star1||star2){
+  if(star1){
     star1 = false;
-    star2 = false;
-  }  else {
+  }  else if(star2&&!const1){
+    if(mouseX>=80&&mouseX<=146&&mouseY>=180&&mouseY<=251){
+      const1 = true;
+    }else {
+      star2 = false;
+    }
+  } else if(star2&&const1){
+    const1 = false;
+  }
+  else {
       if(mouseX>=100&&mouseX<=142&&mouseY>=windowHeight-200&&mouseY<=windowHeight-154){
         star1 = true;
       }
@@ -108,17 +123,33 @@ function mouseClicked(){
 
 function textBoxes(){
   if(star1){
-    background(0,0,20,80)
+    background(0,0,50,180)
     fill(col1);
     rect(windowWidth/2,windowHeight/2,400,250,20);
     fill('white')
     text('seeing the bag charms on different strangers\nswing in sync while walking',windowWidth/2,windowHeight/2+80);
   }
   else if(star2){
-    background(0,0,20,80)
+    background(0,0,50,180)
     fill(col2);
     rect(windowWidth/2,windowHeight/2,400,150,20);
     fill('white')
     text('people using software to do something it\'s not meant to do',windowWidth/2,windowHeight/2);
+
+    image(stars[2],80,180);
+    image(stars[2],240,60);
+    image(stars[2],460,130);
+    image(stars[2],590,370);
   }
+  
+  if(const1){
+      background(0,0,50,180)
+      // fill(col2);
+      // rect(windowWidth/2,windowHeight/2,400,350,20);
+      fill('white')
+      image(const1star1,windowWidth/2-150,windowHeight/2-150);
+      text('making a working calcualtor in Pokemon Pokopia',windowWidth/2,windowHeight/2+170);
+      image(stars[2],80,180);
+  } 
 }
+
